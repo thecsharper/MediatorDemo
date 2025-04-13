5c82df9ce890fcbf17983d1975a6a8cef8f46798
Simon Jones <si@thecsharper.com>
2025-04-13 16:09:18 +0100

    Added demo code

--- MediatorDemo/Program.cs
+++ MediatorDemo/Program.cs
@@ -1,7 +1,5 @@
 using Mediator;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-
+using System.Diagnostics;
 
 // https://github.com/martinothamar/Mediator?tab=readme-ov-file#42-add-mediator-to-di-container
 
@@ -10,8 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 var services = new ServiceCollection(); // Most likely IServiceCollection comes from IHostBuilder/Generic host abstraction in Microsoft.Extensions.Hosting
 
 services.AddMediator();
+
+services.AddSingleton<IPipelineBehavior<Ping, Pong>, PingValidator>();
 var serviceProvider = services.BuildServiceProvider();
 
+var mediator = serviceProvider.GetRequiredService<IMediator>();
+var ping = new Ping(Guid.NewGuid());
+var pong = await mediator.Send(ping);
+Debug.Assert(ping.Id == pong.Id);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -27,28 +31,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public sealed record Ping(Guid Id) : IRequest<Pong>;
+
+public sealed record Pong(Guid Id);
+
+public sealed class PingHandler : IRequestHandler<Ping, Pong>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public ValueTask<Pong> Handle(Ping request, CancellationToken cancellationToken)
+    {
+        return new ValueTask<Pong>(new Pong(request.Id));
+    }
+}
+
+public sealed class PingValidator : IPipelineBehavior<Ping, Pong>
+{
+    public ValueTask<Pong> Handle(Ping request, MessageHandlerDelegate<Ping, Pong> next, CancellationToken cancellationToken)
+    {
+        if (request is null || request.Id == default)
+            throw new ArgumentException("Invalid input");
+
+        return next(request, cancellationToken);
+    }
 }
